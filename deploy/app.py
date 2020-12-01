29995f3a0ad5e3e841aa818c2d658244bee1641c
Janpreet Singh <janpreetsinghgill@gmail.com>
2020-12-01 00:01:20 -0500

    Fixed .gitignore and cleaned-up.

--- deploy/app.py
+++
@@ -1,8 +0,0 @@
-import flask
-from flask import Flask
-
-app = flask.Flask(__name__)
-
-@app.route('/')
-def home():
-    return '<h1>Hello there!</h1><p>This web-page is deployed in Kubernetes on GKE. Please refer to https://github.com/janpreet/K8s-GKE/ for more information</p>'
--- deploy/main.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = "~>0.12"
-  backend "remote" {
-    organization = "Singhventures"
-    workspaces {
-      name = "K8s-GKE-Deploy"
-    }
-  }
-}
